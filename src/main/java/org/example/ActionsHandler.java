383e17cc08e65e3a157f05fa04dcecbb819d2eaa
Evgenia73 <evgeniaborisova02@gmail.com>
2022-09-24 15:16:50 +0500

    Initial commit

---
+++ src/main/java/org/example/ActionsHandler.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class UserMessages {
+    private String message;
+
+    public UserMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String AnswerToUser(String message){
+        if(message.)
+
+
+
+        return message;
+    }
+    public S
+}
